8bbf9a14b4887062273c3ceb4c0a1bc8e6f8e51b
daiwenhui <1946306647@qq.com>
2021-01-18 10:32:45 +0800

    webpack打包其他文件

---
+++ development-other/webpack.config.js
@@ -0,0 +1,34 @@
+const {resolve} = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'built.js',
+        path: resolve(__dirname, 'build')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+
+            {
+                // 打包其他资源
+                // 除了html，css， js
+                exclude: /\.(css|html|js)/, 
+
+                loader: 'file-loader'
+            }
+        ]
+    },
+
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+
+    mode: 'development'
+}
